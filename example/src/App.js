2772c5e33a65362655eaa1d4ebd16d38ba724e3d
Richie <whitedogg13@gmail.com>
2021-05-13 22:06:02 +0800

    feat: support better initial value logic

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
-import {useRevent} from 'revent-lib'
+import {useRevent} from 'revent-lib';
 
 function Add() {
-  const [value, setValue] = useRevent("add", 0);
+  const [value, setValue] = useRevent('add');
   return <button onClick={() => setValue(value + 1)}>+1</button>;
 }
 
 function Sub() {
-  const [value, setValue] = useRevent("add", 0);
+  const [value, setValue] = useRevent('add');
   return <button onClick={() => setValue(value - 1)}>-1</button>;
 }
 
 function Value() {
-  const [value] = useRevent("add", 0);
+  const [value] = useRevent('add');
   return <h1>{value}</h1>;
 }
 
 function App() {
+  useRevent('add', 0);
+
   return (
-    <div style={{ padding: 10 }}>
+    <div style={{padding: 10}}>
       <Value />
       <div>
         <Add />
--- src/index.js
+++ src/index.js
@@ -7,38 +7,46 @@ const Evt = {
   update: 'update',
 };
 
-function EmitterProxy(ee) {
+function EmitterProxy(ee, initialValue) {
+  let innerValue =
+    typeof initialValue === 'function' ? initialValue() : initialValue;
+
   return {
-    onUpdate: (handler) => {
+    register: (handler) => {
       return ee.on(Evt.update, handler);
     },
 
-    offUpdate: (handler) => {
+    unregister: (handler) => {
       return ee.off(Evt.update, handler);
     },
 
     update: (value) => {
+      innerValue = value;
       return ee.emit(Evt.update, value);
+    },
+
+    getValue: () => {
+      return innerValue;
     },
   };
 }
 
-function getProxy(key) {
+function getProxy(key, initialValue) {
   if (!registry.has(key)) {
-    registry.set(key, EmitterProxy(new EventEmitter()));
+    registry.set(key, EmitterProxy(new EventEmitter(), initialValue));
   }
   return registry.get(key);
 }
 
 function useRevent(name, initialValue) {
-  const [value, setValue] = React.useState(initialValue);
-  const eeProxy = getProxy(name);
+  const eeProxy = getProxy(name, initialValue);
+  const [value, setValue] = React.useState(eeProxy.getValue());
 
   React.useEffect(() => {
-    eeProxy.onUpdate(setValue);
+    eeProxy.register(setValue);
 
     return () => {
-      eeProxy.offUpdate(setValue);
+      eeProxy.unregister(setValue);
     };
   }, [eeProxy]);
 
