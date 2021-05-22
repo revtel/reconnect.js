476c1544afebe9a9f970c4095224835956e46efe
Richie <whitedogg13@gmail.com>
2021-05-22 08:59:33 +0800

    doc(style): minor doc styles

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -55,10 +55,12 @@ module.exports = {
         {
           title: 'More',
           items: [
+            /*
             {
               label: 'Blog',
               to: '/blog',
             },
+            */
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
